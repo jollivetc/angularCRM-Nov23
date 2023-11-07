0a0270273514868cf1889e15ba25ba14475b2780
Christophe Jollivet <jollivet@apside.fr>
2023-11-07 10:33:26 +0100

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCRM';
+  cars = ['Citroen', 'Peugeot','Tesla', 'Toyota'];
+
+  onClick($event:MouseEvent):void{
+    console.log($event);
+  }
 }
